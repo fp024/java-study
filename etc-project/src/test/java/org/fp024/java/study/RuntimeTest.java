930c5356981e1fc0629609f22911358a696fd6a3
fp024 <aopenasus@naver.com>
2022-02-02 18:58:40 +0900

    객체 사용 방식으로 바꿨다.

--- etc-project/src/test/java/org/fp024/java/study/RuntimeTest.java
+++ etc-project/src/test/java/org/fp024/java/study/RuntimeTest.java
@@ -1,5 +1,11 @@
 package org.fp024.java.study;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.junit.jupiter.api.Test;
 
 class RuntimeTest {
@@ -19,26 +25,46 @@ class RuntimeTest {
         getMegaByte(freeMemory), getMegaByte(maxMemory), getMegaByte(totalMemory));
     System.out.printf("실제 사용중인 메모리: %,dMB%n", getMegaByte(totalMemory - freeMemory));
     System.out.printf("할당되지 않았으나 앞으로 사용할 수 있는 메모리: %,dMB%n", getMegaByte(maxMemory - totalMemory));
-    System.out.printf("앞으로 사용가능한 메모리: %,dMB%n", getMegaByte(maxMemory - totalMemory + freeMemory));
+    System.out.printf("앞으로 사용 가능한 메모리: %,dMB%n", getMegaByte(maxMemory - totalMemory + freeMemory));
     System.out.println();
   }
 
+  @Getter
+  @Setter
+  @RequiredArgsConstructor
+  @ToString
+  class PersonalInfo {
+    private final String pid;
+    private final String name;
+    private final String phoneNumber;
+  }
+
+  private List<PersonalInfo> makePersonalList() {
+    return IntStream.rangeClosed(1, 10_000_000)
+        .mapToObj(
+            i -> {
+              return new PersonalInfo("000000" + i, "홍길동" + i, "123-123-" + i);
+            })
+        .toList();
+  }
+
   @Test
-  void test() throws InterruptedException {
+  void test() {
     printMemory("초기 상태...");
 
-    long[] longArray = new long[2_141_000_000];
+    @SuppressWarnings("unused")
+    List<PersonalInfo> personalInfoList = makePersonalList();
 
-    printMemory("매우 큰 배열 할당 후...");
+    printMemory("개인정보 목록 생성해서 할당 후...");
 
     Runtime.getRuntime().gc();
-    printMemory("배열 참조를 유지한 채, 명시적 GC 수행 이후...");
+    printMemory("개인정보 목록 참조를 유지한 채, 명시적 GC 수행 이후...");
 
-    longArray = null;
+    personalInfoList = null;
     Runtime.getRuntime().gc();
-    printMemory("배열 참조를 끊은 후 명시적 GC 수행...");
+    printMemory("개인정보 목록 참조를 끊은 후 명시적 GC 수행...");
 
-    longArray = new long[2_142_000_000];
-    printMemory("매우 큰 배열 다시 할당 후...");
+    personalInfoList = makePersonalList();
+    printMemory("개인정보 목록 다시 생성해서 할당 후...");
   }
 }
